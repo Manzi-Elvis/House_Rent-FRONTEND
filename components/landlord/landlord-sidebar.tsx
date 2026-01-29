795a53499356ed4ec12a1dfc96acc030e9434f58
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-29 14:12:13 +0200

    fixed styling issues

--- components/landlord/landlord-sidebar.tsx
+++ components/landlord/landlord-sidebar.tsx
@@ -94,7 +94,7 @@ export function LandlordSidebar() {
             <span className="ml-2 text-xl font-bold">BizRent</span>
           </div>
 
-          <div className="mt-8 flex-grow flex flex-col">
+          <div className="mt-8 grow flex flex-col">
             <nav className="flex-1 px-2 space-y-1">
               {navigation.map((item) => {
                 const isActive = pathname === item.href
